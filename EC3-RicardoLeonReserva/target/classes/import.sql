51edeedbcc946e08b9a644ab383bb00b03c8033d
RicardoLeonGuevara <110881039+RicardoLeonGuevara@users.noreply.github.com>
2022-11-15 22:17:34 -0500

    Add files via upload

---
+++ EC3-RicardoLeonReserva/target/classes/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO hospital (nombre,descripcion,distrito)  VALUES ('Hospital Carrión','Complejo hospitalario público peruano','Bellavista');
+INSERT INTO cliente (nombre,celular) VALUES ('Ricardo León','937642891');
+INSERT INTO usuariocliente (usuario,password,rol) VALUES ('ricardo','$2a$12$Rn4XH5r6RQhY4VHn5Da7POYUYaPR5v1.1o1ZYC1.AO5TRn/e2XQf2','ROL_USER');
